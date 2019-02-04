b899b3e62ed4143f9f19fe96aa8ea2dfae7dc48c
Kevin Su <kevisu@deloitte.com.au>
2019-02-04 11:46:49 +1100

    update

--- app/src/components/Home.js
+++ app/src/components/Home.js
@@ -8,10 +8,7 @@ class Home extends Component {
         <div className="logo-container">
           <img src={logo} className="dd-logo" alt="" />
         </div>
-        <h1 className="App-title">Welcome to the Deloitte Digital FED exercise</h1>
-        <p>You can view the instructions in the <code>README.md</code> file</p>
-        <p>Remember to write any assumptions in an <code>ASSUMPTIONS.md</code> file</p>
-        <p className="warning">Please overwrite the code for this page, we do not expect you to handle any routing in this test</p> 
+        <h1 className="App-title">Welcome</h1>
       </div>
     );
   }
